bf12e7e488db0d49db29e19da40aff41b01e40a8
thomasvaeth <thomas.vaeth@gmail.com>
2015-10-31 17:28:13 -0700

    Added HTML5 geolocation.

---
+++ static/js/geolocation.js
@@ -0,0 +1,15 @@
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		console.log('Geolocation is not supported by this browser.');
+	}
+}
+
+function showPosition(position) {
+	console.log("Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude); 
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	getLocation();
+});
