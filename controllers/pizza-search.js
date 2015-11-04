9fdd2361087fd2f0c2c6a58576217f3109c991cb
thomasvaeth <thomas.vaeth@gmail.com>
2015-11-04 14:08:48 -0800

    Moved files.

--- controllers/pizza-search.js
+++ controllers/pizza-search.js
@@ -13,28 +13,11 @@ var yelp = require('yelp').createClient({
   token_secret: process.env.YELP_TOKEN_SECRET
 });
 
-router.get('/', function(req, res) {
-	if (req.query.address === undefined) {
-		var lat = req.query.latitude;
-		var lng = req.query.longitude;
-		yelp.search({term: 'pizza', category_filter: 'pizza', sort: 1, ll: lat + ',' + lng}, function(error, data) {
-			res.render('pizza/index', {data: data});
-		});
-	} else {
-		request('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.query.address + '&key=' + process.env.GOOGLE_MAPS_SERVER_KEY, function(error, response, body) {
-			if (!error && response.statusCode === 200) {
-				var data = JSON.parse(body);
-				if (data.status === 'OK') {
-					var latLng = data.results[0].geometry.location;
-					yelp.search({term: 'pizza', category_filter: 'pizza', sort: 1, ll: latLng.lat + ',' + latLng.lng}, function(error, data) {
-						res.render('pizza/index', {data: data});
-					});
-				} else {
-					res.redirect('/');
-				}
-			}
-		});
-	}
+router.get('/:idx', function(req, res) {
+	var idx = req.params.idx;
+	yelp.business(idx, function(error, data) {
+	  res.render('pizza/show', {data: data});
+	});
 });
 
 module.exports = router;
